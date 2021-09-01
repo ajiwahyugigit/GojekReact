d6db6fae58d6b31a8139267563061db6f5835413
ajiwahyugi.git <ajiwahyugi.git@gmail.com>
2021-09-01 14:09:24 +0700

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Header} from 'react-native-elements';
+import { Header } from 'react-native-elements';
 
 const Header = () => {
     return (
